beee0c53a29432d14c0612e483c0ee95a49e4aeb
coxlaurel <coxlaurel@gmail.com>
2024-12-10 11:55:05 -0500

    functioning wall jump

--- wall_clip/main.py
+++ wall_clip/main.py
@@ -11,17 +11,17 @@ dt = 0
 
 # Player setup
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player_size = 80
+player_size = 20
 player_color = "blue"
 player_speed = 300
 player = pygame.Rect(player_pos.x, player_pos.y, player_size, player_size)
 
 # Walls
 walls = {
-    "left": pygame.Rect(0, 0, 200, SCREEN_HEIGHT),
-    "right": pygame.Rect(SCREEN_WIDTH - 200, 0, 200, SCREEN_HEIGHT),
-    "top": pygame.Rect(0, 0, SCREEN_WIDTH, 200),
-    "bottom": pygame.Rect(0, SCREEN_HEIGHT - 200, SCREEN_WIDTH, 200),
+    "left": pygame.Rect(SCREEN_WIDTH*2/8, SCREEN_HEIGHT/3, 5, SCREEN_HEIGHT/3),
+    "right": pygame.Rect(SCREEN_WIDTH*6/8 - 5, SCREEN_HEIGHT/3, 5, SCREEN_HEIGHT/3),
+    "top": pygame.Rect(SCREEN_WIDTH*2/8, SCREEN_HEIGHT/3, SCREEN_WIDTH*4/8, 5),
+    "bottom": pygame.Rect(SCREEN_WIDTH*2/8, SCREEN_HEIGHT*2/3, SCREEN_WIDTH*4/8, 5),
 }
 
 while running:
@@ -30,11 +30,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the screen with a color to wipe away anything from last frame
-    screen.fill("grey")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                player_speed = 300
 
     # Reset player color
-    player_color = "blue"
+    player_color = "blue" 
 
     # Handle movement
     keys = pygame.key.get_pressed()
@@ -47,7 +48,8 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += player_speed * dt
     if keys[pygame.K_SPACE]:
-        player_speed = 600
+        player_speed = 500
+        player_color = "green"
 
     # Update player rectangle's position
     player.topleft = (player_pos.x, player_pos.y)
@@ -56,15 +58,11 @@ while running:
     for wall, rectangle in walls.items():
         if player.colliderect(rectangle):
             player_color = "red"
-            if wall == "left":
-                player_pos.x += 300 * dt
-            elif wall == "right":
-                player_pos.x -= 300 * dt
-            elif wall == "top":
-                player_pos.y += 300 * dt
-            elif wall == "bottom":
-                player_pos.y -= 300 * dt
+            player_pos.x = SCREEN_WIDTH/2
+            player_pos.y = SCREEN_HEIGHT/2
 
+    # Draw background
+    screen.fill("grey")
 
     # Draw walls
     for wall in walls.values():
@@ -78,6 +76,6 @@ while running:
 
     # Limit FPS to 60
     # dt is delta time in seconds since the last frame
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(15) / 1000
 
 pygame.quit()
